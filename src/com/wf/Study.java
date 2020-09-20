68364ee5e990aefbe6145c7f626aa794a2144128
Wf <285682283@qq.com>
2020-09-20 21:15:21 +0800

    --2020.10--study--

---
+++ src/com/wf/Study.java
@@ -0,0 +1,12 @@
+package com.wf;
+
+/**
+ * @author wf
+ * @create 2020-09-20 21:11
+ * @desc
+ **/
+public class Study {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
